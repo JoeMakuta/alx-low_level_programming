54e3ba5a7b0500273fb55ed94a9843de52e054d2
Josué Makuta <makutajosue@gmail.com>
2023-03-23 14:59:15 +0200

    Feat : isupper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,14 @@
  * @c: the character to be checked
  * Return: 1 if c is uppercase , 0 otherwise
  */
+
 int _isupper(char c)
 {
-	if (isupper(c))
-		return (1);
-	else
-		return (0);
+	if(isupper(c)!=0)
+	{
+		return 1;
+	}else
+	{
+		return 0;
+	}
 }
